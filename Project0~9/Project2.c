d97d7c6b55db3f3354954e78fb5b7e2860737f56
Minjong-KIM <dure7108@naver.com>
2021-10-11 09:17:17 +0900

    first commit

---
+++ Project0~9/Project2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int apple = 1;
+	char A = 'A';
+	double pi = 3.14;
+	float e = 2.718;
+
+	printf("apple : %d\n", apple);
+	printf("A : %c\n", A);
+	printf("pi : %.2lf\n", pi);
+	printf("e : %.3f\n", e);
+
+	return 0;
+}
