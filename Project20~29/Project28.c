b5c041cf46be881db7888bffa13db484a2ead6da
Minjong-KIM <dure7108@naver.com>
2021-10-14 09:08:07 +0900

    first commit

---
+++ Project20~29/Project28.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int k = 0; k < 4 - i; k++)
+		{
+			printf(" ");
+		}
+
+		for (int j = 0; j < i + 1; j++)
+		{
+			printf("%d", i + 1);
+		}
+
+		printf("\n");
+	}
+}
